8a240652acc8c08c9553c189d8909b3d8512c4ee
ycct <ynk_93@hotmail.com>
2022-03-20 00:05:55 +0300

    verified screen's bugs fixed

--- lib/screens/verified_screen/verified_screen.dart
+++ lib/screens/verified_screen/verified_screen.dart
@@ -12,46 +12,41 @@ class VerifiedScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      resizeToAvoidBottomInset: true,
       body: Stack(
         alignment: Alignment.center,
         children: [
-          SizedBox(
-            height: context.dynamicHeight(1),
-            width: context.dynamicHeight(1),
-            child:
-                SvgPicture.asset(ImagePaths.backgroundImage, fit: BoxFit.cover),
-          ),
-          Positioned(
-            top: context.paddingLargeHeight * 3,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                buildSvgPicture(context),
-                context.sizedBoxHeightSmall,
-                buildText(context),
-                context.sizedBoxHeightExtraSmall,
-              ],
+          Positioned.fill(
+            bottom: -context.dynamicHeight(0.12),
+            child: SvgPicture.asset(
+              ImagePaths.backgroundImage,
+              fit: BoxFit.fitWidth,
             ),
           ),
           Positioned(
-            bottom: context.dynamicHeight(0.16),
+            bottom: context.dynamicHeight(0.55),
+            child: buildSvgPicture(context),
+          ),
+          Positioned(
+            bottom: context.dynamicHeight(0.3),
+            child: buildText(context),
+          ),
+          Positioned(
+            bottom: context.dynamicHeight(0.10),
             child: CustomElevatedButton(
               onTap: () {
                 Get.toNamed("/home");
               },
               title: AppConstants.next,
-              width: context.dynamicWidth(0.72),
-              height: context.dynamicWidth(0.12),
             ),
           ),
         ],
       ),
     );
   }
+
   SizedBox buildSvgPicture(BuildContext context) {
     return SizedBox(
-      height: context.dynamicHeight(0.25),
+      height: context.dynamicHeight(0.3),
       width: context.dynamicWidth(1),
       child: SvgPicture.asset(
         ImagePaths.verified,

ec77c783bae2d7f1e2f175742cebf800cc710a64
ycct <35145117+ycct@users.noreply.github.com>
2022-04-14 14:43:28 +0300

    reset password screen is edited

--- lib/screens/settings_screen/edit_add_screens/reset_password_screen.dart
+++ lib/screens/settings_screen/edit_add_screens/reset_password_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:yallawashtest/extensions.dart';
 import 'package:yallawashtest/screens/settings_screen/edit_add_screens/my_addresses_screen.dart';
+import 'package:yallawashtest/screens/settings_screen/edit_add_screens/update_mail_screen.dart';
 
 class ResetPasswordScreen extends StatelessWidget {
   const ResetPasswordScreen({Key? key}) : super(key: key);
@@ -9,6 +11,16 @@ class ResetPasswordScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: buildAppBarForSecondScreens(context, title: Get.currentRoute.tr, leading: "cancel".tr),
+        body: Padding(
+          padding:  EdgeInsets.symmetric(vertical: context.paddingDefaultHeight),
+          child: ListView(children: [
+            buildCard(context,title: "currentPassword".tr),
+            buildCard(context, title: "newPassword".tr),
+            buildCard(context,title: "confirmPassword".tr),
+          ]),
+        )
+
     );
+
   }
 }
